187f51af443e9e0b61c20e71e0e3c2e8b38bc0c2
Richard Caetano <caetano@gmail.com>
2015-07-31 21:08:34 +0200

    working prototype

---
+++ elastic.js
@@ -0,0 +1,50 @@
+// export SEARCHBOX_URL=http://paas:d7579a8b7c6c6bf7b3e92a3592ad90b8@dwalin-us-east-1.searchly.com
+// export FIREBASE_URL=http://eppocratetest.firebaseio.com
+
+var Firebase = require('firebase');
+var elasticsearch = require('elasticsearch');
+
+var config = {
+    firebaseUrl: process.env.FIREBASE_URL,
+    elasticSearchUrl: process.env.SEARCHBOX_URL
+}
+var rootRef = new Firebase(config.firebaseUrl);
+
+var client = new elasticsearch.Client({
+    host: config.elasticSearchUrl
+});
+
+var usersRef = rootRef.child('articles');
+
+usersRef.on('child_added', upsert);
+usersRef.on('child_changed', upsert);
+usersRef.on('child_removed', remove);
+
+function upsert(snapshot){
+    client.index({
+        index: 'firebase',
+        type: 'articles',
+        id: snapshot.key(),
+        body: snapshot.val()
+    }, function(error, response){
+        if(error){
+            console.log("Error indexing articles : " + error);
+        } else {
+            console.log("Added new article " + snapshot.val().title);
+        }
+    })
+
+}
+
+function remove(snapshot){
+    client.delete({
+        index: 'firebase',
+        type: 'articles',
+        id: snapshot.key()
+    }, function(error, response){
+        if(error){
+            console.log("Error deleting articles : " + error);
+            console.log("Removed new article " + snapshot.val().title);
+        }
+    });
+}
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var express = require('express');
+var elasticsearch = require('elasticsearch');
 var app = express();
+
+
+var config = {
+    firebaseUrl: process.env.FIREBASE_URL,
+    elasticSearchUrl: process.env.SEARCHBOX_URL
+}
+
+var elastic = new elasticsearch.Client({
+    host: config.elasticSearchUrl
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,6 +21,26 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+  response.render('pages/content');
+});
+
+app.get('/search', function(request, response) {
+	// https://www.elastic.co/guide/en/elasticsearch/guide/current/search-lite.html
+	q = request.query.q;
+	elastic.search({
+	    index: 'firebase',
+	    type: 'articles',
+	    body: { query: { query_string: { query:q } } }
+	}).then(function (resp) {
+		response.render('pages/search', {
+			results: resp.hits.hits, query_string: q
+		});
+	}, function (err) {
+	    response.send(err.message);
+	});	
+});
+
+app.get('/tutorial', function(request, response) {
   response.render('pages/index');
 });
 
